ed7d40d9e40bf24aeaca58d56f8a35058e1a2edc
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-01-28 06:30:45 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+
+
+st.title('Mi primera aplicación')
+
+st.sidebar.markdown('# Introducción')
