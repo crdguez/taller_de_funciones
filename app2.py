3f854cd2e9acb0c398590065dcbafc6e94214a76
crdguez <carlos.rodriguez.jaso@gmail.com>
2021-02-01 07:40:47 +0100

    pruebas

--- app2.py
+++ app2.py
@@ -1,6 +1,8 @@
 # app2.py
 import streamlit as st
+import streamlit.components.v1 as components
 
 def app() :
     st.title('Pruebas')
     st.markdown('### Ejemplo de uso de $\LaTeX$')
+    components.iframe("https://www.heraldo.es/", scrolling = True)
