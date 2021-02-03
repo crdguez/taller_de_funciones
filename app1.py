8732c54bcb2a7d6c921eea7d0d9ebf7335e68ef6
crdguez <carlos.rodriguez.jaso@gmail.com>
2021-02-03 23:28:29 +0100

    z

--- app1.py
+++ app1.py
@@ -9,6 +9,15 @@ from sympy.plotting.plot import List2DSeries
 from sympy import *
 x, y, z, t = symbols('x y z t')
 
+def carac(exp) :
+    # Devuelve un diccionario con las características de la función f(x)=exp
+    d = dict()
+    d['exp']=exp
+    d['raices']=solve(exp)
+    d['dominio']=S.Reals - singularities(exp,x)
+    d['fg']= plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10))._backend.fig
+    return d
+
 def app() :
     # El eco es para publicar el código después, se puede borrar y eliminar el tabulado
     # with st.echo('below') :
@@ -30,29 +39,30 @@ def app() :
     with col03 :
         c = st.select_slider('  c',options=list(range(-1*sz,sz+1)),value=1)
 
-    col11, col12 = st.beta_columns([1,2])
-    #Tamaño de los deslizadores
+    ex=r'ax^2+bx+c'
+    #ex=r'\frac{1}{x}'
+    eq = parse_latex(ex).subs('a',a).subs('b',b).subs('c',c)
+
+    col11, col12 = st.beta_columns([1,1])
 
     with col11 :
-        ex=r'ax^2+bx+c'
-        eq = parse_latex(ex).subs('a',a).subs('b',b).subs('c',c)
-        st.markdown('**Función**')
-        st.latex('y='+latex(eq))
-        st.markdown('**Raíces**')
-        st.write(r' $'+ latex(eq)+r'\to$' )
-        st.write(r'$'+r', '.join(map(latex,solve(eq)))+r'$')
-        st.markdown('**Dominio**')
-        st.write(r'$'+latex(S.Reals - singularities(eq,x))+'$')
+        d = carac(eq)
+        st.write("**Características**:  \n * Función:  \n     *  $f(x) ="+ \
+        latex(d['exp'])+"$  \n * Corte OX: $"+", ".join(map(latex,d['raices']))+ \
+        "$  \n * Dominio: $"+latex(d['dominio'])+"$")
 
     with col12 :
         # Graficamos la función
-        p1 = plot_implicit(Eq(y,eq), (x, -10, 10), (y, -10, 10))
-        fg =  p1._backend.fig
-        st.pyplot(fg)
+        st.pyplot(d['fg'])
+
+
+
 
     #st.markdown('---')
     #st.markdown('Código python usando la librería *streamlit*:')
     # Fin de echo, Si está dentro de un *echo* aparecerá el código
+
+
 
     st.subheader('Estudiando el dominio')
     col21, col22 = st.beta_columns(2)
@@ -65,10 +75,10 @@ def app() :
 
     col31, col32, col33 = st.beta_columns([6,1,3])
     with col31 :
-        p2 = plot_implicit(Eq(y,eq), (x, -5, 5), (y,-10, 10), how=False)
+        p2 = plot_implicit(Eq(y,eq), (x, -5, 5), (y,-10, 10), show=False)
         p2.extend(plot_implicit(Eq(x,v), (x, -5, 5), (y, -10, 10), show=False))
 
-        vx, vy =list(solve([Eq(y,eq),Eq(x,v)]).values()) if degree(eq, gen=x) ==1 else list(solve([Eq(y,eq),Eq(x,v)])[0].values())
+        vx, vy =list(solve([Eq(y,eq),Eq(x,v)]).values()) if degree(eq, gen=x) == 1 else list(solve([Eq(y,eq),Eq(x,v)])[0].values())
         p2.append(List2DSeries([vx-0.1,vx+0.1],[vy,vy]))
         p2.show()
         fg =  p2._backend.fig
