cd18e71bb5246e95e6acf0e991fb5de005590a3c
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-02-03 09:48:08 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -75,10 +75,10 @@ def app() :
     with col32 :
         st.latex(r'\to')
     with col33 :
-        txt = r""" El punto
+        txt = r""" El punto <br>
         $\left("""+latex(vx)+r','+latex(vy)+r"""\right)$ pertenece a la gráfica. Por
         tanto:"""
-        st.markdown(txt)
+        st.markdown(txt,unsafe_allow_html=True)
         txt = r"""- $"""+latex(vx)+""" \in Dom(f)$ y"""
         st.markdown(txt)
         txt = r"""- $"""+latex(vy)+""" \in Im(f)$ """
