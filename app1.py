5502554f185a79919b04b3edb581507ff5579d7c
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-02-03 10:15:16 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -79,12 +79,12 @@ def app() :
         tanto:  \n * $"""+latex(vx)+""" \in Dom(f)$ y  \n * $"""+latex(vy)+""" \in Im(f)$ """
         st.markdown(txt)
        
-    st.write("Not multi-\nline")
-    st.write("Still not multi- \nline")
-    st.write("Ok now it's multi-  \nline")
-    st.markdown("Ok now it's multi-  \nline  \n * a ver  \n * a ver 2")
-    txt = """ El punto $\left("""+latex(vx)+r','+latex(vy)+r"\right)$"+""" pertenece a la gráfica. Por
-        tanto:  \n * $"""+latex(vx)+""" \in Dom(f)$ y  \n * $"""+latex(vy)+""" \in Im(f)$ """
-    st.markdown(txt)
+#     st.write("Not multi-\nline")
+#     st.write("Still not multi- \nline")
+#     st.write("Ok now it's multi-  \nline")
+#     st.markdown("Ok now it's multi-  \nline  \n * a ver  \n * a ver 2")
+#     txt = """ El punto $\left("""+latex(vx)+r','+latex(vy)+r"\right)$"+""" pertenece a la gráfica. Por
+#         tanto:  \n * $"""+latex(vx)+""" \in Dom(f)$ y  \n * $"""+latex(vy)+""" \in Im(f)$ """
+#     st.markdown(txt)
     
-    st.markdown("Ok now it's multi-  \nline  \n * a ver  \n * a ver 2")
+#     st.markdown("Ok now it's multi-  \nline  \n * a ver  \n * a ver 2")
