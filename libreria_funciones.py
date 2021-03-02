e39342c0b9836ab238b891586afd14442c7b2b1c
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-03-02 09:13:47 +0100

    Update libreria_funciones.py

--- libreria_funciones.py
+++ libreria_funciones.py
@@ -43,7 +43,8 @@ def carac(exp,tipo) :
         # d['extra']['pendiente']= "$"+latex(Poly(exp,x).LC())+"$"
         # d['extra']['ordenada']= "$"+latex(Poly(exp,x).TC())+"$"
         v0=-1*Poly(exp,x).all_coeffs()[1]/(2*Poly(exp,x).all_coeffs()[0])
-        d['extra']['Vértice']= "$"+latex(v0)+"$"
+#         d['extra']['Vértice']= "$"+latex(v0)+"$"
+        d['extra']['Vértice']= "$"+latex(max_min(exp)['maxmin'])+"$"
         d['forma']='La **gráfica** de la función es una **parábola**. '
 
 
