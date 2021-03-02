d435e8ec870ba7a50740e2d35bf99d2278b5acff
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-03-02 09:50:16 +0100

    Update libreria_funciones.py

--- libreria_funciones.py
+++ libreria_funciones.py
@@ -20,10 +20,10 @@ def carac(exp,tipo) :
     d['dominio']=continuous_domain(exp,x,S.Reals)
     d['rango']=function_range(exp,x,S.Reals)
 #     plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10))._backend.fig
-    p=plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10), adaptative=False)
+    p=plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10))
     fg, ax = p._backend.fig, p._backend.ax 
     ax[0].set_aspect('equal')
-#     plt.grid=True
+    plt.grid(True)
     d['fg']= fg
     d['poly']=exp.is_polynomial()
 
