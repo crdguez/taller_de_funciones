fbca98344e05931fd38835a3b653adaab30bdcae
Crdguez <31032709+crdguez@users.noreply.github.com>
2021-03-02 09:54:29 +0100

    Update libreria_funciones.py

--- libreria_funciones.py
+++ libreria_funciones.py
@@ -20,9 +20,7 @@ def carac(exp,tipo) :
     d['dominio']=continuous_domain(exp,x,S.Reals)
     d['rango']=function_range(exp,x,S.Reals)
 #     plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10))._backend.fig
-    plt.grid(True)
     p=plot_implicit(Eq(y,exp), (x, -10, 10), (y, -10, 10))
-    
     fg, ax = p._backend.fig, p._backend.ax 
     ax[0].set_aspect('equal')
     
